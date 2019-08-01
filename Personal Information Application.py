7f914d2dceefc39fdd4e370551dbb1e05ea027bd
Gulcan1 <gulcan.som@gmail.com>
2019-08-01 23:12:55 +0300

    Add files via upload

---
+++ Personal Information Application.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+isim="Aslı"
+soyisim="Erdoğan"
+doğum="1985"
+doğum_yeri="İzmir"
+print("===============================")
+print("İsim:", isim)
+print("Soyisim:",soyisim)
+print("Doğum Yılı:",doğum)
+print("Doğum Yeri:",doğum_yeri)
+print("===============================")
+
+
+# In[ ]:
+
+
+
+
